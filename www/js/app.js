231ff2f99feeb39a6be60a34969f37b085957e93
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-04-28 22:40:23 +0200

    Add config for views.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,53 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    $stateProvider.state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.todolists', {
+        url: '/todolists',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/todolists.html',
+                controller: 'TodolistCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-todolists" class="button button-fab button-fab-top-right expanded button-energized-900 flap"><i class="icon ion-paper-airplane"></i></button>',
+                controller: function ($timeout) {
+                    $timeout(function () {
+                        document.getElementById('fab-todolists').classList.toggle('on');
+                    }, 200);
+                }
+            }
+        }
+    })
+
+    .state('app.todos', {
+        url: '/todos',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/todos.html',
+                controller: 'TodosCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-todos" class="button button-fab button-fab-top-left expanded button-energized-900 spin"><i class="icon ion-chatbubbles"></i></button>',
+                controller: function ($timeout) {
+                    $timeout(function () {
+                        document.getElementById('fab-todos').classList.toggle('on');
+                    }, 900);
+                }
+            }
+        }
+    })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/todolists');
+});
